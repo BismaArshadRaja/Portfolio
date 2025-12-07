d31f7c3db56ba133dfede0f960375c94a3ea1938
Raja-Yaseen <162034747+Raja-Yaseen@users.noreply.github.com>
2025-12-07 22:45:37 +0500

    curd table

---
+++ script.js
@@ -0,0 +1,9 @@
+// open button click
+document.getElementById("open").addEventListener("click", function() {
+    document.getElementById("formfiller").style.display = "block";
+});
+
+// close button click (X)
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("formfiller").style.display = "none";
+});
